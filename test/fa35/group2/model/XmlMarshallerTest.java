f79674a7ce98dd8197065232b9adf0879f979407
fireandfuel <fireandfuel@hotmail.de>
2015-11-09 10:55:35 +0100

    Fixed group number in XmlMarshallerTest

--- test/fa35/group2/model/XmlMarshallerTest.java
+++ test/fa35/group2/model/XmlMarshallerTest.java
@@ -1,7 +1,7 @@
 package fa35.group2.model;
 
 
-import fa35.group1.model.xml.XmlMarshaller;
+import fa35.group2.model.xml.XmlMarshaller;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
